aa74cf4161d6093769a0d9da5d0a7e08d0b9058b
Matha Charan <mathacharan3004@gmail.com>
2024-10-25 16:38:45 +0530

    added auth

--- stampsSphere/src/App.jsx
+++ stampsSphere/src/App.jsx
@@ -8,36 +8,41 @@ import Sidebar from './components/Sidebar.jsx';
 import Header from './components/Header.jsx';
 import Login from './pages/login.jsx';
 import Register from './pages/Register.jsx';
+import { store } from './store';
+import { Provider } from 'react-redux';
 const App = () => {
   return (
+
     <div className="h-screen flex overflow-hidden">
-      {/* Sidebar - Fixed on the Left */}
-      <div className="fixed left-0 top-0 h-full w-72 bg-custom-red">
-        <Sidebar />
-      </div>
-
-      {/* Main Content - Adjusted for Sidebar */}
-      <div className="ml-72 flex-1 flex flex-col">
-        {/* Fixed Header */}
-        <div className="fixed top-0 left-72 right-0 h-16 bg-white shadow">
-          <Header />
+      <Provider store={store}>
+        {/* Sidebar - Fixed on the Left */}
+        <div className="fixed left-0 top-0 h-full w-72 bg-custom-red">
+          <Sidebar />
         </div>
 
-        {/* Main Content - Below Header */}
-        <div className="flex-1 overflow-auto pt-16">
-          <Routes>
-            <Route path="/livebid" element={<Livebid />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/categories/:id" element={<Categories />} />
-            <Route path="/dashboard/admin" element={<AdminDashboard />} />
-          </Routes>
-        </div>
+        {/* Main Content - Adjusted for Sidebar */}
+        <div className="ml-72 flex-1 flex flex-col">
+          {/* Fixed Header */}
+          <div className="fixed top-0 left-72 right-0 h-16 bg-white shadow">
+            <Header />
+          </div>
 
-        {/* Footer - Below Main Content */}
-        <Footer />
-      </div>
+          {/* Main Content - Below Header */}
+          <div className="flex-1 overflow-auto pt-16">
+            <Routes>
+              <Route path="/livebid" element={<Livebid />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/categories/:id" element={<Categories />} />
+              <Route path="/dashboard/admin" element={<AdminDashboard />} />
+            </Routes>
+          </div>
+
+          {/* Footer - Below Main Content */}
+          <Footer />
+        </div>
+      </Provider>
     </div>
   );
 };
---
+++ stampsSphere/src/slices/AuthSlice.js
@@ -0,0 +1,61 @@
+//working fine 
+//common axios is working 
+//use that 
+import { createSlice ,createAsyncThunk} from "@reduxjs/toolkit";
+import axios from '../axios.jsx';
+import { useNavigate } from "react-router-dom";
+
+
+export const loginUser=createAsyncThunk('loginUser',async(data)=>{
+    const response = await axios.post("/api/auth/login",data);
+    console.log(response); 
+    return response.data;
+}) 
+
+export const logoutUser=createAsyncThunk('logoutUser',async()=>{
+    console.log("logout dispatch");
+    const response=await axios.get("/api/auth/logout");
+    console.log(response.data);  
+})
+ 
+
+const authSlice=createSlice({
+    name:'authUser',
+    initialState: {
+        user: null,
+        status: 'idle',
+      },
+    extraReducers:(builder)=>{
+        builder.addCase(loginUser.pending,(state,action)=>{
+            state.request=true;
+        })
+        builder.addCase(loginUser.fulfilled,(state,action)=>{
+            state.request=false;
+            state.status='succeded';
+            state.error=false;
+            state.success=true;
+            state.user = action.payload;
+        })
+        builder.addCase(loginUser.rejected,(state,action)=>{
+            state.request=false;
+            state.error=true;
+            state.success=false;
+        })
+        builder.addCase(logoutUser.pending,(state,action)=>{
+            state.request=true;
+        })
+        builder.addCase(logoutUser.fulfilled,(state,action)=>{
+            state.request=false;
+            state.error=false;
+            state.success=false;
+            state.user = null;
+        })
+        builder.addCase(logoutUser.rejected,(state,action)=>{
+            state.request=false;
+            state.error=true;
+            state.success=false;
+        })
+    }
+})
+
+export default authSlice.reducer;
