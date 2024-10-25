c40f1e097470dd7ea71c3fcc9aa6fcf2b2b6a642
CHIRANTH RAJU C <chiranthrajuc@gmail.com>
2024-10-25 18:16:14 +0530

    landing page

--- stampsSphere/src/components/Sidebar.jsx
+++ stampsSphere/src/components/Sidebar.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+
 import Logo from "../assets/CategoryList/StampSpherebg.png";
 const Sidebar = () => {
   return (
     <div className="p-4">
       {/* Logo Section */}
       <div className="flex justify-center items-center mb-6">
-        <img 
-          src={Logo}  // Replace with your logo URL 
-          alt="Logo" 
-          className="h-60 w-auto object-contain rounded-full" // Adjust size as needed 
-        />
+        <a href="/">
+          <img
+            src={Logo} // Replace with your logo URL
+            alt="Logo"
+            className="h-60 w-auto object-contain rounded-full" // Adjust size as needed
+          />
+        </a>
       </div>
 
       {/* Navigation Menu */}
       <nav className="mt-6 text-xl text-white">
         <ul>
-          <li className="my-2 p-2 border-b">Categories</li>
+          <li className="my-2 p-2 border-b">
+            <a href="/categories/1">Categories</a>
+          </li>
           <li className="my-2 p-2 border-b">Auctions</li>
           <li className="my-2 p-2 border-b">Community</li>
         </ul>
--- stampsSphere/src/components/header.jsx
+++ stampsSphere/src/components/header.jsx
@@ -2,15 +2,13 @@ import { Button, Avatar } from "@material-tailwind/react";
 import { useState } from "react";
 
 export default function Header() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true); // Example state for login
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Example state for login
 
   return (
     <header className="bg-white shadow-md dark:bg-gray-800 p-4">
       <div className="flex justify-between items-center mx-6">
         {/* Title Section */}
-        <h1 className="text-xl font-semibold dark:text-white">
-          StampSphere
-        </h1>
+        <h1 className="text-xl font-semibold dark:text-white">StampSphere</h1>
 
         {/* Login/Signup or Profile Avatar */}
         <div className="flex items-center space-x-4">
@@ -23,9 +21,11 @@ export default function Header() {
           ) : (
             <>
               <Button variant="outlined" className="mr-2">
-                Login
+                <a href="/login">Login</a>
               </Button>
-              <Button variant="outlined">SignUp</Button>
+              <Button variant="outlined">
+                <a href="/register">Register</a>
+              </Button>
             </>
           )}
         </div>
